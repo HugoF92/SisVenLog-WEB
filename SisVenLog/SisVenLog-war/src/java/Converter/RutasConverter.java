12549a2fc8d225526c6f80be2b6a8dd7c9321398
nickolaz <nicoayalakoy@gmail.com>
2020-02-01 10:44:39 -0300

    generacion de datos de venta por zona

---
+++ SisVenLog/SisVenLog-war/src/java/Converter/RutasConverter.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Converter;
+
+import entidad.Rutas;
+import entidad.RutasPK;
+import java.io.Serializable;
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.inject.Named;
+
+
+@Named(value = "RutasConverter")
+@ApplicationScoped
+
+public class RutasConverter  implements Converter, Serializable{
+    
+   @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value)
+    {
+        if (value.trim().equals(""))
+        {
+            return null;
+        }
+        else
+        {
+            return new Rutas(new RutasPK(new Short("2"), Short.parseShort(value)));
+        }
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value)
+    {
+        if (value == null || value.equals(""))
+        {
+            return "";
+        }
+        else
+        {
+            return String.valueOf(((Rutas) value).getRutasPK().getCodRuta());
+        }
+    }
+
+    
+}
