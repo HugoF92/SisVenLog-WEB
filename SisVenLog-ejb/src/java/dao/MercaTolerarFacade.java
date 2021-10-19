91cb8e53b7d6eeab6c14e2ef297544978998c659
Federico Cano <fcano70@gmail.com>
2021-10-18 21:15:48 -0300

    se ajusto el facade con la logica requerida

--- SisVenLog-ejb/src/java/dao/MercaTolerarFacade.java
+++ SisVenLog-ejb/src/java/dao/MercaTolerarFacade.java
@@ -9,6 +9,7 @@ import entidad.MercaTolerar;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.StoredProcedureQuery;
 
 /**
  *
@@ -28,5 +29,47 @@ public class MercaTolerarFacade extends AbstractFacade<MercaTolerar> {
     public MercaTolerarFacade() {
         super(MercaTolerar.class);
     }
-    
+/*
+   public void insertarMercaTolerar(MercaTolerar mercatolerar) {
+
+        StoredProcedureQuery q = getEntityManager().createStoredProcedureQuery("InsertarMercaTolerar");
+        q.registerStoredProcedureParameter("xdesc", String.class, ParameterMode.IN);
+        q.registerStoredProcedureParameter("cod_zona", String.class, ParameterMode.IN);
+        q.registerStoredProcedureParameter("mtipo", Character.class, ParameterMode.IN);
+        q.registerStoredProcedureParameter("npeso_max", Double.class, ParameterMode.IN);
+        q.registerStoredProcedureParameter("fultim_inven", Date.class, ParameterMode.IN);
+        q.registerStoredProcedureParameter("falta", Date.class, ParameterMode.IN);
+        q.registerStoredProcedureParameter("cusuario", String.class, ParameterMode.IN);
+        q.registerStoredProcedureParameter("fultim_modif", Date.class, ParameterMode.IN);
+        q.registerStoredProcedureParameter("cusuario_modif", String.class, ParameterMode.IN);
+        q.registerStoredProcedureParameter("npunto_est", Integer.class, ParameterMode.IN);
+        q.registerStoredProcedureParameter("npunto_exp", Integer.class, ParameterMode.IN);
+        q.registerStoredProcedureParameter("cod_conductor", Integer.class, ParameterMode.IN);
+        q.registerStoredProcedureParameter("cod_transp", Integer.class, ParameterMode.IN);
+        q.registerStoredProcedureParameter("xchapa", String.class, ParameterMode.IN);
+        q.registerStoredProcedureParameter("xmarca", String.class, ParameterMode.IN);
+        q.registerStoredProcedureParameter("xobs", String.class, ParameterMode.IN);
+        
+        q.setParameter("xdesc", depositos.getXdesc());
+        q.setParameter("cod_zona", depositos.getZonas().getZonasPK().getCodZona());
+        q.setParameter("mtipo", depositos.getMtipo());
+        q.setParameter("npeso_max", depositos.getNpesoMax());
+        q.setParameter("fultim_inven", depositos.getFultimInven());
+        q.setParameter("falta", depositos.getFalta());
+        q.setParameter("cusuario", depositos.getCusuario());
+        q.setParameter("fultim_modif", depositos.getFultimModif());
+        q.setParameter("cusuario_modif", depositos.getCusuarioModif());
+        q.setParameter("npunto_est", depositos.getNpuntoEst());
+        q.setParameter("npunto_exp", depositos.getNpuntoExp());
+        q.setParameter("cod_conductor", depositos.getCodConductor().getCodConductor());
+        q.setParameter("cod_transp", depositos.getCodTransp().getCodTransp());
+        q.setParameter("xchapa", depositos.getXchapa());
+        q.setParameter("xmarca", depositos.getXmarca());
+        q.setParameter("xobs", depositos.getXobs());
+        
+        q.execute();
+
+        
+    } 
+*/
 }
