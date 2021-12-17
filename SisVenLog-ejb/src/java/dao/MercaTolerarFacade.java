7aa44f4af4bb7b2ccec8635a85a4c73f8bb48e8c
Federico Cano <fcano70@gmail.com>
2021-12-17 17:16:33 -0300

    ultimos ajustes

--- SisVenLog-ejb/src/java/dao/MercaTolerarFacade.java
+++ SisVenLog-ejb/src/java/dao/MercaTolerarFacade.java
@@ -3,17 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  
- */
+*/
 package dao;
 
 import entidad.MercaTolerar;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-import util.DateUtil;
 
 /**
  *
@@ -33,31 +30,15 @@ public class MercaTolerarFacade extends AbstractFacade<MercaTolerar> {
     public MercaTolerarFacade() {
         super(MercaTolerar.class);
     }
-
-    public List<MercaTolerar> obtenerListadoMercaTolerar(long lNroFactura, Short lCodProveedor, String lCodMerca) {
-        String sql = "SELECT * "
-                + "FROM merca_tolerar "
-                + "WHERE nrofact = " + lNroFactura + " "
-                + "AND cod_empr = 2 "
-                + "AND cod_proveed = " + lCodProveedor + " "
-                + "and upper(cod_merca) like upper('" + lCodMerca + "') ";
+    
+    public List<MercaTolerar> obtenerListadoMercaTolerar(long lNroFactura, Short lCodProveedor, String lCodMerca){
+        String sql =    "SELECT * " +
+                        "FROM merca_tolerar " +
+                        "WHERE nrofact = "+lNroFactura+" "+
+                        "AND cod_empr = 2 " +
+                        "AND cod_proveed = "+lCodProveedor+" "+
+                        "and upper(cod_merca) like upper('"+lCodMerca+"') ";
         System.out.println(getEntityManager().createNativeQuery(sql, MercaTolerar.class));
         return getEntityManager().createNativeQuery(sql, MercaTolerar.class).getResultList();
-    }
-
-    public void newInsertarMercaTolerar(MercaTolerar mercatolerar) throws Exception {
-        EntityTransaction trx = getEntityManager().getTransaction();
-        try {
-            trx.begin();
-            String sql = "";
-
-            Query q = getEntityManager().createNativeQuery(sql);
-            q.executeUpdate();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            trx.rollback();
-            throw e;
-        }
     }
 }
